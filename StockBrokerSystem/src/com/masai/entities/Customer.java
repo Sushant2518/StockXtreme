1f8776f452b426a985d31127b57d9fb2fb2088da
sushant2518 <sushantnarvekar3118@gmail.com>
2023-05-04 23:46:24 +0530

    final done

--- StockBrokerSystem/src/com/masai/entities/Customer.java
+++ StockBrokerSystem/src/com/masai/entities/Customer.java
@@ -1,5 +1,37 @@
 package com.masai.entities;
+import java.io.Serializable;
 
-public class Customer {
+public class Customer extends User implements Serializable{
+private double walletBalance;
+	
+	
 
+	public Customer(double walletBalance,String username,String password,String email) {
+		super(username, password, email);
+		this.walletBalance = walletBalance;
+	}
+
+
+
+	public double getWalletBalance() {
+		return walletBalance;
+	}
+
+
+
+	public void setWalletBalance(double walletBalance) {
+		this.walletBalance = walletBalance;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Customer [walletBalance=" + walletBalance + ", getWalletBalance()=" + getWalletBalance()
+				+ ", getUsreanme()=" + getUsername() + ", getPassword()=" + getPassword() + ", getEmail()=" + getEmail()
+				+ ", toString()=" + super.toString() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ "]";
+	}
+	
+	
 }
